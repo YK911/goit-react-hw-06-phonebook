f958872f126aba32fcdaec94d27544f2ff94396a
YK911 <jaroslavkositsa@gmail.com>
2020-03-01 19:43:59 +0200

    change 'name' line in Readme

---
+++ build/precache-manifest.383b83d08b7738f5e78caec2060cc708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d521b5f19a06db2529d9c699468c4541",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "03f844bc45704179c36c",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.4f35cf17.chunk.css"
+  },
+  {
+    "revision": "1b23db83addab9df2c61",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.e05f3a9d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.e05f3a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f844bc45704179c36c",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.78c0712b.chunk.js"
+  },
+  {
+    "revision": "0d4d37491f8621c9f163",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.78193737.js"
+  }
+]);
